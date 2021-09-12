332824b469fe4f265582ff53de4156a4d53383c7
bensammwaniki <bensammwaniki@gmail.com>
2021-09-13 00:39:00 +0300

    populating aticles

--- news/main/view.py
+++ news/main/view.py
@@ -8,9 +8,11 @@ from ..request import get_sources,get_articles
 def index():
     title = 'This is the best News Center'
     news_sources = get_sources('general')
-    return render_template('index.html',title = title,sources =news_sources)
-@main.route('/articles/<id>')
-
-def articles(id):    
     articles = get_articles(id)
-    return render_template('articles.html',id = id, articles=articles)    
+
+
+    return render_template('index.html',title = title,sources =news_sources,articles=articles)
+# @main.route('/articles/<id>')
+
+# def articles(id):    
+#     return render_template('articles.html',id = id, articles=articles)    
